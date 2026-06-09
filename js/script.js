154b132fceb033ffd737c27c8d42f04414bbd591
Shridhar0903 <bagayatkarshridhar@gmail.com>
2026-06-09 11:45:12 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const slider = document.getElementById("servicesSlider");
+
+document.querySelector(".next-btn").addEventListener("click", () => {
+  slider.scrollBy({
+    left: slider.clientWidth,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector(".prev-btn").addEventListener("click", () => {
+  slider.scrollBy({
+    left: -slider.clientWidth,
+    behavior: "smooth",
+  });
+});
+
+// ============================================================================
+
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((btn) => btn.classList.remove("active"));
+
+    contents.forEach((content) => content.classList.remove("active"));
+
+    tab.classList.add("active");
+
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
+
+//==================Hero Slider ==============================================================
+
+const slides = document.querySelectorAll(".hero-slide");
+const indicators = document.querySelectorAll(".indicator");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  indicators.forEach((indicator) => indicator.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  indicators[index].classList.add("active");
+
+  currentSlide = index;
+}
+
+/* Indicator Click */
+
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener("click", () => {
+    showSlide(index);
+  });
+});
+
+/* Auto Slide */
+
+setInterval(() => {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+}, 5000);
